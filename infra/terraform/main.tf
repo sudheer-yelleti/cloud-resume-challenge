67dc8853a3a6053be6431b5e10f59793e9c6571a
sudheer <sudheer.167@gmail.com>
2025-09-16 03:02:06 -0700

    Add initial version of terraform config to provision azure resources

--- infra/terraform/main.tf
+++ infra/terraform/main.tf
@@ -1,1 +1,55 @@
+# Retrieve the current client configuration to get the Tenant ID
+data "azurerm_client_config" "current" {}
+
+# Create a new Management Group under the Root Tenant Group
+resource "azurerm_management_group" "management_groups" {
+for_each = {
+  "dev" = "cloud-resume-challenge-dev"
+  "prod" = "cloud-resume-challenge-prod"
+}
+  display_name             = each.value
+  parent_management_group_id = null
+}
+
+# Create subscriptions for each environment
+resource "azurerm_subscription" "subscriptions" {
+  for_each = {
+    dev  = "cloud-resume-challenge-dev"
+    prod = "cloud-resume-challenge-prod"
+  }
+  billing_scope_id = data.azurerm_billing_mca_account_scope.billing.id
+  subscription_name = each.value
+}
+
+data "azurerm_billing_mca_account_scope" "billing" {
+  billing_account_name = "12cd9329-fb91-54d2-9782-c40a099cd0d6:41addaf6-2c7d-47f4-a271-ed76e330c27f_2019-05-31"
+billing_profile_name = " Sudheer Yelleti"
+invoice_section_name = "Sudheer Yelleti"
+}
+
+# Associate subscriptions with management groups
+resource "azurerm_management_group_subscription_association" "associations" {
+  for_each = {
+    dev  = "dev"
+    prod = "prod"
+  }
+  
+  management_group_id = azurerm_management_group.management_groups[each.key].id
+  subscription_id     = azurerm_subscription.subscriptions[each.key].id
+}
+
+# Create resource groups in each subscription
+resource "azurerm_resource_group" "resource_groups" {
+  for_each = {
+    dev  = "cloud-resume-challenge-dev-rg"
+    prod = "cloud-resume-challenge-prod-rg"
+  }
+  
+  name     = each.value
+  location = "East US"
+  
+  # Use the subscription provider alias if needed
+  depends_on = [azurerm_subscription.subscriptions]
+}
+
 
---
+++ infra/terraform/providers.tf
@@ -0,0 +1,16 @@
+# We strongly recommend using the required_providers block to set the
+# Azure Provider source and version being used
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "=4.1.0"
+    }
+  }
+}
+
+# Configure the Microsoft Azure Provider
+provider "azurerm" {
+  features {}
+  subscription_id = "8362a864-1a8b-49e7-86c1-9cfdf8850b33"
+}
